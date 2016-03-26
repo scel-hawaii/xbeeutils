553fc9b23c659d4872216d6f8863cefe82ca8d36
Kenny Luong <luong97@hawaii.edu>
2016-03-25 23:25:37 -1000

    Removed old code

--- collector/wbschema.py
+++ collector/wbschema.py
@@ -74,17 +74,6 @@ def parse(unpack_fmt, data, headers):
     return d
 
 
-'''
-    unpack_fmt = '<'
-    for f in fmt:
-        unpack_fmt += f['size']*f['instance'];
-    print unpack_fmt
-
-    headers = []
-    for d in fmt:
-        headers.append(d['name'])
-    print headers
-'''
 
 if __name__ == '__main__':
     print "Finished loading schemas"
